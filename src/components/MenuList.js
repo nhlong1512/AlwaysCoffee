d4be491d00285f111ab2c628e01bb5749b3c15b3
UIT20521569 <20521569@gm.uit.edu.vn>
2022-02-27 14:58:02 +0700

    Test Responsive

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Menu from './Menu'
 function MenuList({menuList}) {
   return (
-    <div className="row flex flex-wrap mb-[24px]">
+    <div className="row flex flex-col md:flex-row flex-wrap mb-[24px]">
       {menuList.map(menu => (
         <Menu
           key = {menu.id}
