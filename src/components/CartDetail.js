1081ab447d08237fc38804b6cea542b6b2220c66
UIT20521569 <20521569@gm.uit.edu.vn>
2022-03-31 02:25:11 +0700

    Add Cart Detail

--- src/components/CartDetail.js
+++ src/components/CartDetail.js
@@ -1,12 +1,13 @@
 import React, { useContext, useState } from 'react'
 import InitContext from '../store/InitContext'
+import { IoTrashOutline } from "react-icons/io5";
 
 function CartDetail() {
   const init = useContext(InitContext)
   const itemsPrice = init.cartItems.reduce((a, c) => a + c.qty * c.price, 0)
   { console.log(init.cartItems) }
   return (
-    <div className="bg-[#f0f0f0]">
+    <div className="bg-[#f0f0f0] pb-[80px]">
       {init.cartItems.length === 0 &&
         (<div className="pt-[120px] mx-auto flex flex-col justify-center items-center">
           <div>
@@ -23,22 +24,64 @@ function CartDetail() {
       {
         init.cartItems.length !== 0 &&
         (
-          <div className="h-[100%] bg-[#fff] rounded-[8px] pt-[120px]">
-            {init.cartItems.map((item) => (
-              <div key = {item.id} className="flex py-[20px]">
-                <div className="flex basis-[8%] justify-center items-center ">
+          <div className="pt-[120px]">
+            <div className="row h-[100%] rounded-[16px] bg-[#fff] ">
+              {init.cartItems.map((item) => (
+                <div >
+                  <div key={item.id} className="flex py-[20px] bg-[#fff]">
+                    <div className="flex basis-[8%] justify-center items-center ">
 
+                    </div>
+                    <div className="p-0 flex justify-center items-center basis-[16%]">
+                      <a href="">
+                        <img src={item.image} alt={item.name} className="" />
+                      </a>
+                    </div>
+                    <div className="basis-[68%] flex">
+                      <div className="flex basis-[65%] px-[20px] py-[10px] flex-col justify-between">
+                        <div className="w-[100%]">
+                          <a>{item.name}</a>
+                        </div>
+                        <div>
+                          <h2 className="font-bold">{item.price}</h2>
+                        </div>
+                      </div>
+                      <div className="flex basis-[35%] m-auto justify-center items-center">
+                        <div className="flex-[2] px-[10px]">
+                          <div className="h-[30px] border-E0E rounded-[5px] flex justify-center items-center ">
+                            <button
+                              className="px-[6px] text-[30px] leading-[30px]"
+                              onClick={() => init.onRemove(item)}
+                            >
+                              -
+                            </button>
+                            <h2>{item.qty}</h2>
+                            <button
+                              className="px-[6px] text-[24px] leading-[30px]"
+                              onClick={() => init.onAdd(item)}
+                            >
+                              +
+                            </button>
+                          </div>
+                        </div>
+                        <div className="flex flex-[3] items-center justify-center">
+                          <h2 className="text-[16px] text-primary-color font-bold">{item.price * item.qty}</h2>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex basis-[8%] items-center justify-center">
+                      <a className="text-[24px]">
+                        <IoTrashOutline />
+                      </a>
+                    </div>
+                  </div>
+                  <div className="border-b-custom-95 flex items-center justify-center mx-auto">
+
+                  </div>
                 </div>
-                <div className="p-0 flex justify-center items-center basis-[16%]">
-                  <a href="">
-                    <img src={item.imgae} alt={item.name} className=""/>
-                  </a>
-                </div>
-                <div className="basis-[68%] flex">
-                  {/* <div className="flex"></div> Đang làm */}
-                </div>
-              </div>
-            ))}
+              ))}
+
+            </div>
           </div>
         )
       }
