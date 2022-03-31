1c843a199e084d214fb51de036ab123ff6a7870c
UIT20521569 <20521569@gm.uit.edu.vn>
2022-04-01 02:29:30 +0700

    Update UI price

--- src/components/CartDetail.js
+++ src/components/CartDetail.js
@@ -7,17 +7,17 @@ function CartDetail() {
   const itemsPrice = init.cartItems.reduce((a, c) => a + c.qty * c.price, 0)
   const onAdd = (item) => {
     const exist = init.cartItems.find(x => x.id === item.id)
-    if(exist) {
-      init.setCartItems(init.cartItems.map(x => x.id === item.id ? {...exist, qty: exist.qty + 1} : x))
+    if (exist) {
+      init.setCartItems(init.cartItems.map(x => x.id === item.id ? { ...exist, qty: exist.qty + 1 } : x))
     } else {
-      init.setCartItems([...init.cartItems, {...item, qty: 1}])
+      init.setCartItems([...init.cartItems, { ...item, qty: 1 }])
     }
   }
 
   const onRemove = (item) => {
     const exist = init.cartItems.find((x) => x.id === item.id);
     if (exist.qty === 1) {
-      init.setCartItems(init.cartItems.filter((x) => x.id !== item.id));
+      return;
     } else {
       init.setCartItems(
         init.cartItems.map((x) =>
@@ -25,10 +25,14 @@ function CartDetail() {
         )
       );
     }
-  };
-  { console.log(init.cartItems) }
+  }
+
+  const onDelete = (item) => {
+    init.setCartItems(init.cartItems.filter((x) => x.id !== item.id));
+  }
   return (
-    <div className="bg-[#f0f0f0] pb-[80px]">
+    <div className="bg-[#f0f0f0] pb-[80px] pt-[120px] ">
+
       {init.cartItems.length === 0 &&
         (<div className="pt-[120px] mx-auto flex flex-col justify-center items-center">
           <div>
@@ -45,8 +49,11 @@ function CartDetail() {
       {
         init.cartItems.length !== 0 &&
         (
-          <div className="pt-[120px] flex justify-center items-center">
-            <div className="h-[100%] rounded-[16px] bg-[#fff] row">
+          <div className="flex justify-center items-center">
+            <div className="h-[100%] rounded-[16px] row">
+              <div className="mb-[24px] text-[18px]">
+                GIỎ HÀNG ({init.cartItems.length} sản phẩm)
+              </div>
               {init.cartItems.map((item) => (
                 <div key={item.id}>
                   <div className="flex py-[20px] bg-[#fff]">
@@ -60,25 +67,25 @@ function CartDetail() {
                     </div>
                     <div className="basis-[68%] flex">
                       <div className="flex basis-[65%] px-[20px] py-[10px] flex-col justify-between">
-                        <div className="w-[100%]">
+                        <div className="w-[100%] font-semibold hover:text-primary-color">
                           <a>{item.name}</a>
                         </div>
                         <div>
-                          <h2 className="font-bold">{item.price}</h2>
+                          <h2 className="font-bold hover:text-primary-color">{item.price.toLocaleString()}</h2>
                         </div>
                       </div>
                       <div className="flex basis-[35%] m-auto justify-center items-center">
                         <div className="flex-[2] px-[10px]">
                           <div className="h-[30px] border-E0E rounded-[5px] flex justify-center items-center ">
                             <button
-                              className="px-[6px] text-[30px] leading-[30px]"
+                              className="px-[6px] text-[30px] leading-[30px] hover:text-primary-color"
                               onClick={() => onRemove(item)}
                             >
                               -
                             </button>
                             <h2>{item.qty}</h2>
                             <button
-                              className="px-[6px] text-[24px] leading-[30px]"
+                              className="px-[6px] text-[24px] leading-[30px] hover:text-primary-color"
                               onClick={() => onAdd(item)}
                             >
                               +
@@ -86,12 +93,15 @@ function CartDetail() {
                           </div>
                         </div>
                         <div className="flex flex-[3] items-center justify-center">
-                          <h2 className="text-[16px] text-primary-color font-bold">{item.price * item.qty}</h2>
+                          <h2 className="text-[16px] text-primary-color font-bold">{(item.price * item.qty).toLocaleString()} đ</h2>
                         </div>
                       </div>
                     </div>
-                    <div className="flex basis-[8%] items-center justify-center">
-                      <a className="text-[24px]">
+                    <div
+                      className="flex basis-[8%] items-center justify-center cursor-pointer"
+                      onClick={() => onDelete(item)}
+                    >
+                      <a className="text-[24px] hover:text-primary-color">
                         <IoTrashOutline />
                       </a>
                     </div>
