be9599ed40a8b02c795ec1324b42ac954c164ec0
UIT20521569 <20521569@gm.uit.edu.vn>
2022-04-03 19:17:38 +0700

    Update Checkbox for item in itemDetail

--- src/components/CartDetail.js
+++ src/components/CartDetail.js
@@ -58,72 +58,87 @@ function CartDetail() {
         init.cartItems.length !== 0 &&
         (
           <div>
-            <div className="row mb-[24px] text-[18px]">
+            <div className="row mb-[16px] text-[20px]">
               GIỎ HÀNG ({init.cartItems.length} sản phẩm)
             </div>
             <div className="row flex justify-center">
               {/* Section Detail Cart */}
-              <div className="w-[66.666%] bg-[#fff] rounded-[8px]">
-
-                {init.cartItems.map((item) => (
-                  <div key={item.id}>
-                    <div className="flex py-[20px]">
-                      <div className="flex basis-[8%] justify-center items-center ">
-
-                      </div>
-                      <div className="p-0 flex justify-center items-center basis-[16%]">
-                        <a href="">
-                          <img src={item.image} alt={item.name} className="" />
-                        </a>
-                      </div>
-                      <div className="basis-[68%] flex">
-                        <div className="flex basis-[65%] px-[20px] py-[10px] flex-col justify-between">
-                          <div className="w-[100%] font-semibold hover:text-primary-color">
-                            <a>{item.name}</a>
-                          </div>
-                          <div>
-                            <h2 className="font-bold hover:text-primary-color">{item.price.toLocaleString()} đ</h2>
-                          </div>
+              <div className="w-[66.666%] rounded-[8px]">
+                <div className="flex mb-[16px] rounded-[8px] py-[10px] bg-[#fff]">
+                  <div className="flex basis-[8%] justify-center items-center ">
+                    <input type="checkbox" className="checkbox-add-cart" />
+                  </div>
+                  <div className="flex basis-[62%] font-medium">
+                    Chọn Tất Cả ({init.cartItems.length} sản phẩm)
+                  </div>
+                  <div className="flex basis-[13%] font-medium">
+                    Số Lượng
+                  </div>
+                  <div className="flex basis-[17%] font-medium">
+                    Thành Tiền
+                  </div>
+                </div>
+                <div className="bg-[#fff]">
+                  {init.cartItems.map((item) => (
+                    <div key={item.id}>
+                      <div className="flex py-[20px]">
+                        <div className="flex basis-[8%] justify-center items-center ">
+                          <input type="checkbox" className="checkbox-add-cart" />
                         </div>
-                        <div className="flex basis-[35%] m-auto justify-center items-center">
-                          <div className="flex-[2] px-[10px]">
-                            <div className="h-[30px] border-E0E rounded-[5px] flex justify-center items-center ">
-                              <button
-                                className="px-[6px] text-[30px] leading-[30px] hover:text-primary-color flex justify-center items-center text-center m-auto"
-                                onClick={() => onRemove(item)}
-                              >
-                                -
-                              </button>
-                              <input type="text"
-                                value={item.qty}
-                                className="px-[4px] w-[30px] flex justify-center items-center text-center border-E0E-x h-[100%]"
-                              />
-                              <button
-                                className="px-[6px] text-[24px] leading-[30px] hover:text-primary-color"
-                                onClick={() => onAdd(item)}
-                              >
-                                +
-                              </button>
+                        <div className="p-0 flex justify-center items-center basis-[16%]">
+                          <a href="">
+                            <img src={item.image} alt={item.name} className="" />
+                          </a>
+                        </div>
+                        <div className="basis-[68%] flex">
+                          <div className="flex basis-[65%] px-[20px] py-[10px] flex-col justify-between">
+                            <div className="w-[100%] font-semibold hover:text-primary-color">
+                              <a>{item.name}</a>
+                            </div>
+                            <div>
+                              <h2 className="font-bold hover:text-primary-color">{item.price.toLocaleString()} đ</h2>
                             </div>
                           </div>
-                          <div className="flex flex-[3] items-center justify-center">
-                            <h2 className="text-[16px] text-primary-color font-bold">{(item.price * item.qty).toLocaleString()} đ</h2>
+                          <div className="flex basis-[35%] m-auto justify-center items-center">
+                            <div className="flex-[2] px-[10px]">
+                              <div className="h-[30px] border-E0E rounded-[5px] flex justify-center items-center ">
+                                <button
+                                  className="px-[6px] text-[30px] leading-[30px] hover:text-primary-color flex justify-center items-center text-center m-auto"
+                                  onClick={() => onRemove(item)}
+                                >
+                                  -
+                                </button>
+                                <input type="text"
+                                  value={item.qty}
+                                  className="px-[4px] w-[30px] flex justify-center items-center text-center border-E0E-x h-[100%]"
+                                />
+                                <button
+                                  className="px-[6px] text-[24px] leading-[30px] hover:text-primary-color"
+                                  onClick={() => onAdd(item)}
+                                >
+                                  +
+                                </button>
+                              </div>
+                            </div>
+                            <div className="flex flex-[3] items-center justify-center">
+                              <h2 className="text-[16px] text-primary-color font-bold">{(item.price * item.qty).toLocaleString()} đ</h2>
+                            </div>
                           </div>
                         </div>
+                        <div
+                          className="flex basis-[8%] items-center justify-center cursor-pointer"
+                          onClick={() => onDelete(item)}
+                        >
+                          <a className="text-[24px] hover:text-primary-color">
+                            <IoTrashOutline />
+                          </a>
+                        </div>
                       </div>
-                      <div
-                        className="flex basis-[8%] items-center justify-center cursor-pointer"
-                        onClick={() => onDelete(item)}
-                      >
-                        <a className="text-[24px] hover:text-primary-color">
-                          <IoTrashOutline />
-                        </a>
-                      </div>
+                      <div className="border-product"></div>
                     </div>
-                    <div className="border-product"></div>                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
-
               {/* Section Payment */}
               <div className="w-[33.333%] pl-[24px] ">
                 <div className="bg-[#fff] rounded-[8px] pt-[12px] pb-[12px]">
