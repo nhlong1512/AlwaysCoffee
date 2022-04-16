e509713f00fd949e18dc2f0e2d3918fd8809998d
nhlong1512 <20521569@gm.uit.edu.vn>
2022-04-16 16:56:17 +0700

    add button

--- src/components/AlwaysStories.js
+++ src/components/AlwaysStories.js
@@ -1,6 +1,8 @@
-import React from 'react';
-
+import { React, useContext } from 'react';
+import { Link } from "react-router-dom"
+import InitContext from '../store/InitContext'
 function AlwaysStories() {
+  const init = useContext(InitContext)
   return (
     <div className="bg-[#f0f0f0] pb-[80px] pt-[120px] flex justify-center"
     >
@@ -9,8 +11,17 @@ function AlwaysStories() {
         <div className="w-[588px] max-w-[100%] mx-auto text-center mb-[24px]">
           <p className="text-[16px] leading-[20px] text-center">Always Coffe là nơi mọi người xích lại gần nhau hơn, đề cao giá trị kết nối con người và chia sẻ thân tình bên những tách cà phê, ly trà đượm hương, truyền cảm hứng về lối sống hiện đại.</p>
         </div>
-        <ul>
-
+        <ul className="flex justify-center items-center">
+          {init.storyItems.map(storyItem => (
+            <Link
+              to={storyItem.link}
+              className="px-[12px]"
+              key={storyItem.id}
+              onClick={() => init.setActiveId(storyItem.id)}
+            >
+              <a href="" className="button-tab-story">{storyItem.content}</a>
+            </Link>
+          ))}
         </ul>
       </div>
     </div>
